280253d6e7ed94d8e4893b06a107e41972535e6a
sudo-shaka <68864205+sudo-shaka@users.noreply.github.com>
2021-06-24 17:04:21 -0400

    Add files via upload

---
+++ main.m
@@ -0,0 +1,70 @@
+clear;
+
+pkg load image;
+
+IMG = imread("Sample_Images/IMG.png");
+IMG_MASK = im2bw(IMG,0.1);
+
+[CENTERS,RADII] = imfindcircles(IMG_MASK,[15 50],'Sensitivity',0.9);
+
+NUCLEII = [];
+
+for ii = 1:length(CENTERS)
+	center = CENTERS(ii,:);
+	overlapped = CENTERS(min((abs(center-CENTERS) < 2*RADII(ii))'),:);
+	avg_overlapped = mean(overlapped);
+
+	calculated = max(min(avg_overlapped' == reshape(NUCLEII,2,[])));
+
+	if length(avg_overlapped) == 1
+		NUCLEII = [NUCLEII,center];
+	elseif ~calculated
+		NUCLEII = [NUCLEII,center];
+	end
+end
+
+NUCLEII = round(reshape(NUCLEII,2,[])');
+
+len = length(NUCLEII);
+
+fprintf("%d nucleii found.\n",len);
+
+DATA = zeros(10,length(NUCLEII));
+LENGTHS = zeros(2,length(NUCLEII));
+
+figure; hold on;
+
+imshow(IMG_MASK);
+for i = 1:length(NUCLEII)
+	x1 = NUCLEII(i,1);
+	y1 = NUCLEII(i,2);
+	angles = [0,pi/2,pi,3*pi/2];
+	DATA(1,i) = x1; DATA(2,i) = y1;
+    
+	for ii = 1:length(angles)
+		pixel = 1;
+		angle = angles(ii);
+		x2 = x1; y2 = y1;
+		while pixel
+			try
+				pixel = IMG_MASK(round(y2),round(x2));
+				x2 = x2 + round(cos(angle));
+				y2 = y2 + round(sin(angle));
+                		plot(x2,y2,'.');
+			catch
+				break;
+			end
+		end
+		DATA(ii*2+1,i) = x2;
+		DATA(ii*2+2,i) = y2;
+	end
+
+	LENGTHS(1,i) = DATA(3,i) - DATA(7,i);
+	LENGTHS(2,i) = DATA(6,i) - DATA(10,i);
+end
+
+hold off;
+close all;
+
+disp(LENGTHS(1,:)./LENGTHS(2,:));
+
